43d73344d33fc9b414098fe299355ce9675a66e1
Christian Ramelow <info@codekandis.net>
2021-01-30 21:43:41 +0100

    Updates the tests bootstrap script.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,13 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\GrabDaTapes\Tests;
 
+use function dirname;
 use function error_reporting;
 use function ini_set;
 use const E_ALL;
 
 /**
  * Represents the bootstrap script of the tests.
- *
  * @package codekandis/grabdatapes
  * @author  Christian Ramelow <info@codekandis.net>
  */
@@ -15,4 +15,4 @@ error_reporting( E_ALL );
 ini_set( 'display_errors', 'On' );
 ini_set( 'html_errors', 'Off' );
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once dirname( __DIR__, 1 ) . '/vendor/autoload.php';
