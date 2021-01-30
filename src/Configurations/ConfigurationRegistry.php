2d0df9d66a61cb6bc6e41c02b1594525bfe92079
Christian Ramelow <info@codekandis.net>
2021-01-30 21:50:57 +0100

    Updates the class `ConfigurationRegistry`.

--- src/Configurations/ConfigurationRegistry.php
+++ src/Configurations/ConfigurationRegistry.php
@@ -1,33 +1,38 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\GrabDaTapes\Configurations;
 
-use CodeKandis\Tiphy\Configurations\AbstractConfigurationRegistry;
+use CodeKandis\TiphySentryClientIntegration\Configurations\AbstractConfigurationRegistry;
 use function dirname;
 
 class ConfigurationRegistry extends AbstractConfigurationRegistry
 {
-	/** @var string */
-	private $webDirectoryConfigurationPath = '';
-
-	/** @var WebDirectoryConfigurationInterface */
-	private $webDirectoryConfiguration;
+	/** @var ?WebDirectoryConfigurationInterface */
+	private ?WebDirectoryConfigurationInterface $webDirectoryConfiguration = null;
 
 	protected function initialize(): void
 	{
-		$this->setRoutesConfigurationPath( dirname( __DIR__, 2 ) . '/config/routes.php' );
-		$this->setPersistenceConfigurationPath( dirname( __DIR__, 2 ) . '/config/persistence.php' );
-		$this->setTemplateRendererConfigurationPath( dirname( __DIR__, 2 ) . '/config/templateRenderer.php' );
-		$this->setWebDirectoryConfigurationPath( dirname( __DIR__, 2 ) . '/config/webDirectory.php' );
-	}
-
-	public function setWebDirectoryConfigurationPath( string $path ): void
-	{
-		$this->webDirectoryConfigurationPath = $path;
+		$this->setPlainSentryClientConfiguration(
+			require dirname( __DIR__, 2 ) . '/config/sentryClient.php'
+		);
+		$this->setPlainRoutesConfiguration(
+			( require dirname( __DIR__, 2 ) . '/config/routes.php' )
+			+ ( require __DIR__ . '/Plain/routes.php' )
+		);
+		$this->setPlainTemplateRendererConfiguration(
+			require __DIR__ . '/Plain/templateRenderer.php'
+		);
+		$this->setPlainWebDirectoryConfiguration(
+			require dirname( __DIR__, 2 ) . '/config/webDirectory.php'
+		);
 	}
 
 	public function getWebDirectoryConfiguration(): WebDirectoryConfigurationInterface
 	{
-		return $this->webDirectoryConfiguration
-		       ?? $this->webDirectoryConfiguration = new WebDirectoryConfiguration( $this->webDirectoryConfigurationPath );
+		return $this->webDirectoryConfiguration;
+	}
+
+	public function setPlainWebDirectoryConfiguration( array $plainWebDirectoryConfiguration ): void
+	{
+		$this->webDirectoryConfiguration = new WebDirectoryConfiguration( $plainWebDirectoryConfiguration );
 	}
 }
