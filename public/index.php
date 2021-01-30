ad293dc625ce510e372a82141502fe899b9f6cc5
Christian Ramelow <info@codekandis.net>
2021-01-30 21:51:24 +0100

    Updates the application bootstrapper.

--- public/index.php
+++ public/index.php
@@ -2,24 +2,33 @@
 namespace CodeKandis\GrabDaTapes;
 
 use CodeKandis\GrabDaTapes\Configurations\ConfigurationRegistry;
+use CodeKandis\GrabDaTapes\Throwables\Handlers\InternalServerErrorThrowableHandler;
+use CodeKandis\SentryClient\SentryClient;
 use CodeKandis\Tiphy\Actions\ActionDispatcher;
 use function error_reporting;
 use function ini_set;
 use const E_ALL;
-use const E_STRICT;
 
 /**
  * Represents the bootstrap script of the project.
- *
  * @package codekandis/grabdatapes
  * @author  Christian Ramelow <info@codekandis.net>
  */
-error_reporting( E_ALL | E_STRICT );
+error_reporting( E_ALL );
 ini_set( 'display_errors', 'On' );
 ini_set( 'html_errors', 'Off' );
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$config           = ConfigurationRegistry::_()->getRoutesConfiguration();
-$actionDispatcher = new ActionDispatcher( $config );
+/** @var ConfigurationRegistry $configurationRegistry */
+$configurationRegistry = ConfigurationRegistry::_();
+
+$sentryClient = new SentryClient( $configurationRegistry->getSentryClientConfiguration() );
+$sentryClient->register();
+
+$actionDispatcher = new ActionDispatcher(
+	$configurationRegistry->getRoutesConfiguration(),
+	null,
+	new InternalServerErrorThrowableHandler( $sentryClient )
+);
 $actionDispatcher->dispatch();
